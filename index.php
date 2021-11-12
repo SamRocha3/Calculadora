8b975cc3afb64244904901625d6442008b960373
Samuel Rocha <ascrocha29@hotmail.com>
2021-11-12 15:24:37 +0000

    final commit

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 require_once("calc/calculator.php");
 
-$numb1 = readline("Qual o primeiro número:");
-$numb2 = readline("Qual o segundo número:");
+$numb1 = (float)readline("Qual o primeiro número:");
+$numb2 = (float)readline("Qual o segundo número:");
 $op = readline("Qual a Operação a realizar (ex: '+','-','/','*'):");
 $cal = $numb1.$op.$numb2;
 try {
